299904c106c725e75e3b1c48e50ab863926a17ee
DioknoMyracel <myraceldiokno28@gmail.com>
2022-04-07 11:35:32 +0800

    second commit, song 1

---
+++ song1.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> MELOPHILE STATION </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="indexto.php">Heaven Knows</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">I'd Rather</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Driver's License</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song3.php">This City</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song4.php">214</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Heaven By Your Side</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melophile Station</h1>
+                <p class="lead">Did you know?? MELOPHILE is the term for those who love music? If you are one of them, then this page is for you. Enjoy! :)</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>I'd Rather</h2>";
+                        echo "<p class='lead '>Luther Vandross</p>";
+                        echo "<p> [Intro] </p>";
+                        echo "<p>I thought sometime alone</p>";
+                        echo "<p>Was what we really needed</p>";
+                        echo "<p>You said this time would hurt more than it helps</p>";
+                        echo " <p>But I couldn't see that</p>";
+                        echo " <p>I thought it was the end</p>";
+                        echo " <p>Of a beautiful story</p>";
+                        echo " <p>And so I left the one I love at home to be alone (alone)</p>";
+                        echo " <p>And I tried to find</p>";
+                        echo " <p>Out if this one thing is true</p>";
+                        echo " <p>That I'm nothing without you</p>";
+                        echo " <p>I know better now</p>";
+                        echo " <p> And I've had the change of heart</p>";
+                        echo " <p> [Chorus]</p>";
+                        echo " <p> I'd rather have bad time with you</p>";
+                        echo " <p>Than good times with someone else</p>";
+                        echo " <p>I'd rather be beside you in a storm </p>";
+                        echo " <p> Than safe and sound by myself</p>";
+                        echo " <p> I'd rather have hard times together</p>";
+                        echo " <p> Than to have it easy apart</p>";
+                        echo " <p> I'd rather have the one who holds my heart</p>";
+                        echo " <p> Whoo-oo-oo-oo yeah</p>";
+                        echo " <p> [Verse]</p>";
+                        echo " <p> And then I met someone</p>";
+                        echo " <p> And thought she could replace you</p>";
+                        echo " <p> We got along just fine</p>";
+                        echo " <p> We wasted time, because she was not you</p>";
+                        echo " <p> We got alot of fun</p>";
+                        echo " <p> Though we knew we were faking </p>";
+                        echo " <p> Love was not impressed with our connection they were all lies, all lies</p>";
+                        echo " <p> So I'm here cause I found this one thing is true</p>";
+                        echo " <p> That I'm nothing withot  you </p>";
+                        echo " <p> i know  better now</p>";
+                        echo " <p> and I've had a change of hearts</p>";
+                        echo " <p> [Chorus]</p>";
+                        echo " <p> I'd rather have bad time with you</p>";
+                        echo " <p>Than good times with someone else</p>";
+                        echo " <p>I'd rather be beside you in a storm </p>";
+                        echo " <p> Than safe and sound by myself</p>";
+                        echo " <p> I'd rather have hard times together</p>";
+                        echo " <p> Than to have it easy apart</p>";
+                        echo " <p> I'd rather have the one who holds my heart</p>";
+                        echo " <p> [Verse]</p>";
+                        echo " <p> I can't blame you if you turn away from me, like I've done you,</p>";
+                        echo " <p>I can only prove the things I say with time,</p>";
+                        echo " <p>Please be mine</p>";
+                        echo " <p> [Chorus]</p>";
+                        echo " <p> I'd rather have bad time with(please be mine) you</p>";
+                        echo " <p>Than good times with someone else(I know)</p>";
+                        echo " <p>I'd rather be beside you in a storm(anytime) </p>";
+                        echo " <p> Than safe and sound by myself(so sure baby)</p>";
+                        echo " <p> I'd rather have hard times together</p>";
+                        echo " <p> Than to have it easy apart</p>";
+                        echo " <p> I'd rather have the one who holds my heart(my heart)</p>";
+                        echo " <p> I'd rather have bad time with you(surely)</p>";
+                        echo " <p>Than good times with someone else(surely)</p>";
+                        echo " <p>I'd rather be beside you in a storm (oh  yeah)</p>";
+                        echo " <p> Than safe and sound by myself(all by myself)</p>";
+                        echo " <p> I'd rather have hard times together</p>";
+                        echo " <p> Than to have it easy apart(you know it)</p>";
+                        echo " <p> I'd rather have the one who holds my heart</p>";
+                        echo " <p> I'd rather have the one who holds my heart</p>";
+                        echo " <p> I'd rather have the one who holds my heart</p>";
+                        echo " <p> whoooo.....who holds my heart</p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Myracel Diokno</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
