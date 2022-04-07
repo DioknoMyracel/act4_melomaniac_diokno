6777c1cc82b8363667cfa781026985acb1d0fee6
DioknoMyracel <myraceldiokno28@gmail.com>
2022-04-07 10:52:24 +0800

    My first commit in webpage index

---
+++ indexto.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> MELOPHILE STATION </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Heaven Knows</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">I'd Rather</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Driver's License</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">This City</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">214</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song6.php">Heaven By Your Side</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melophile Station</h1>
+                <p class="lead">Did you know?? MELOPHILE is the term for those who love music? If you are one of them, then this page is for you. Enjoy! :)</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Heaven Knows</h2>";
+                        echo "<p class='lead '>Rick  Price</p>";
+                        echo "<p> [Intro] </p>";
+                        echo "<p>She's always on my mind</p>";
+                        echo "<p>From the time I awake up,</p>";
+                        echo "<p>'till I close my eyes</p>";
+                        echo " <p>She's everywhere I go</p>";
+                        echo " <p>She's all I know</p>";
+                        echo " <p> And though she's so far away</p>";
+                        echo " <p>It just keeps gettin' stronger everyday</p>";
+                        echo " <p> And even now she's gone</p>";
+                        echo " <p>I'm still holding on</p>";
+                        echo " <p>so tell me where do I start</p>";
+                        echo " <p> 'Cause it's breakin' my heart</p>";
+                        echo " <p> Don't wanna let her go</p>";
+                        echo " <p> [Chorus]</p>";
+                        echo " <p> Maybe my love will come back someday</p>";
+                        echo " <p>Only heaven knows</p>";
+                        echo " <p>And maybe our hearts will find a way </p>";
+                        echo " <p> Only heaven knows</p>";
+                        echo " <p> And all I can do is hope  and pray</p>";
+                        echo " <p> 'Cause heaven knows</p>";
+                        echo " <p> [Verse]</p>";
+                        echo " <p> My friends  keep telling me</p>";
+                        echo " <p> That if ypu really love her</p>";
+                        echo " <p> You've gotta set her free</p>";
+                        echo " <p> And if she returns in kind</p>";
+                        echo " <p> I know she's mine</p>";
+                        echo " <p> But tell me  where do I start </p>";
+                        echo " <p> 'Cause its breaking my heart</p>";
+                        echo " <p> Don't wanna let her go</p>";
+                        echo " <p> [Chorus]</p>";
+                        echo " <p> Maybe my love will come back someday</p>";
+                        echo " <p>Only heaven knows</p>";
+                        echo " <p>And maybe our hearts will find a way </p>";
+                        echo " <p> Only heaven knows</p>";
+                        echo " <p> And all I can do is hope  and pray</p>";
+                        echo " <p> 'Cause heaven knows</p>";
+                        echo " <p> [Verse]</p>";
+                        echo " <p> Why I live in despair</p>";
+                        echo " <p> 'Cause wide  awake or dreaming</p>";
+                        echo " <p> I know she's  never there</p>";
+                        echo " <p> And all the time I act so brave </p>";
+                        echo " <p> I'm shakin' inside</p>";
+                        echo " <p> Why does it hurt me so</p>";
+                         echo " <p> [Chorus]</p>";
+                        echo " <p> Maybe my love will come back someday</p>";
+                        echo " <p>Only heaven knows</p>";
+                        echo " <p>And maybe our hearts will find a way </p>";
+                        echo " <p> Only heaven knows</p>";
+                        echo " <p> And all I can do is hope  and pray</p>";
+                        echo " <p> 'Cause heaven knows</p>";
+                        echo " <p> Heaven knows</p>";
+                        echo " <p> Heaven knows<p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        echo " <p> </p>";
+                        
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Myracel Diokno</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
