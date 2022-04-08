15f5d329c3695e1b59f8a6ef0249277c9e635c93
DioknoMyracel <myraceldiokno28@gmail.com>
2022-04-08 22:27:25 +0800

    commit for song 5

---
+++ song5.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="index.php#page-top"> MELOPHILE STATION </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Heaven Knows</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">I'd Rather</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Driver's License</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">This City</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">214</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song.php">Heaven By Your Side</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h19v class="fw-bolder">Welcome to Melophile Station</h1>
+                <p class="lead">Did you know?? MELOPHILE is the term for those who love music? If you are one of them, then this page is for you. Enjoy! :)</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>214</h2>";
+                        echo "<p class='lead '>Rivermaya</p>";
+                        echo "<p> [Intro] </p>";
+                        echo "<p>Am I real?</p>";
+                        echo "<p>Do the words I speak before you make you feel</p>";
+                        echo "<p>That the love I have for you will see no ending?</p>";
+                        echo " <p>Well, if you look into my eyes, then you should know</p>";
+                        echo " <p>That there is nothing here to doubt, nothing to fear</p>";
+                        echo " <p>And you can lay your questions down 'cause if you'll hold me</p>";
+                        echo " <p>We can fade into the night, and you'll know</p>";
+                        echo " <p> [Chorus]</p>";
+                        echo " <p> The world could die, and everything may lie</p>";
+                        echo " <p> Still, you shan't cry</p>";
+                        echo " <p> 'Cause time may pass, but longer than it'll last</p>";
+                        echo " <p> I'll be by your side</p>";
+                        
+                        echo " <p> [Verse]</p>";
+                        echo " <p> Take my hand</p>";
+                        echo " <p> And gently close your eyes so you could understand</p>";
+                        echo " <p> That there's no greater love tonight than what I've for you</p>";
+                        echo " <p> Well, if you feel the same way for me, then let go</p>";
+                        echo " <p> We can journey to a garden no one knows</p>";
+                        echo " <p> Life is short, my darling, tell me that you love me</p>";
+                        echo " <p>So we can fade into the night, and you'll know</p>";
+                        
+                        echo " <p> [Chorus]</p>";
+                        echo " <p> The world could die, and everything may lie</p>";
+                        echo " <p> Still, you shan't cry</p>";
+                        echo " <p> 'Cause time may pass, but longer than it'll last</p>";
+                        echo " <p> I'll be by your side</p>";
+                        echo " <p> I want you to know</p>";
+                        echo " <p> The world could die, and everything may lie</p>";
+                        echo " <p> Still, you shan't cry</p>";
+                        echo " <p> 'Cause time may pass, but longer than it'll last</p>";
+                        echo " <p> I'll be by your side,forever by your side</p>";
+                        
+                        echo " <p>Forever by your side </p>";
+                        echo " <p>So you won't cry</p>";
+                        
+                        
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Myracel Diokno</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
