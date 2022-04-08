b116e7418d887c9439fb8c7827c6da4eb0d4ee08
DioknoMyracel <myraceldiokno28@gmail.com>
2022-04-08 22:27:48 +0800

    commit for song 6

---
+++ song6.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="index.php#page-top"> MELOPHILE STATION </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Heaven Knows</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">I'd Rather</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Driver's License</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">This City</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">214</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song.php">Heaven By Your Side</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h19v class="fw-bolder">Welcome to Melophile Station</h1>
+                <p class="lead">Did you know?? MELOPHILE is the term for those who love music? If you are one of them, then this page is for you. Enjoy! :)</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Heaven By Your Side</h2>";
+                        echo "<p class='lead '>A1</p>";
+                        echo "<p> [Verse 1] </p>";
+                        echo "<p>You and I cannot hide</p>";
+                        echo "<p>The love we feel inside</p>";
+                        echo "<p>The words we need to say</p>";
+                        echo " <p>I feel that I have always walked alone</p>";
+                        echo " <p>But now that you're here with me</p>";
+                        echo " <p>There'll always be a place that I can go</p>";
+                        echo " <p> [Pre-Chorus]</p>";
+                        echo " <p>Suddenly our destiny has started to unfold</p>";
+                        echo " <p>When you're next to me</p>";
+                        echo " <p>I can see the greatest story love has ever told</p>";
+                        echo " <p> [Chorus]</p>";
+                        echo " <p> Now my life is blessed with the love of an angel</p>";
+                        echo " <p> How can it be true</p>";
+                        echo " <p>Somebody to keep the dream alive </p>";
+                        echo " <p> The dream I found in you</p>";
+                        echo " <p> I always thought the love would be the strangest thing to me</p>";
+                        echo " <p> But when we touch</p>";
+                        echo " <p> I realize that I found my place in heaven by your side</p>";
+                        echo " <p> [Verse 2]</p>";
+                        echo " <p> I could fly when you smile</p>";
+                        echo " <p> I'd walk a thousand miles</p>";
+                        echo " <p> To hear you call my name</p>";
+                        echo " <p> Now that I have finally found the one</p>";
+                        echo " <p> Who will be there for me ,eternally</p>";
+                        echo " <p> My everlasting sun</p>";
+                        echo " <p> [Pre-Chorus]</p>";
+                        echo " <p>Suddenly our destiny has started to unfold</p>";
+                        echo " <p>Heaven by your side</p>";
+                        
+                        echo " <p>That I've found my place in heaven by your side</p>";
+                        echo " <p>Heaven by your side</p>";
+                        echo " <p>Heaven by your side</p>";
+                        echo " <p>Heaven by your side</p>";
+                        echo " <p>And when you're next to me</p>";
+                        echo " <p>I can see the greatest story love has ever told</p>";
+                        echo " <p> Now my life is blessed with the love of an angel</p>";
+                        echo " <p> How can it be true</p>";
+                        echo " <p>Somebody to keep the dream alive </p>";
+                        echo " <p> The dream I found in you</p>";
+                        echo " <p> I always thought the love would be the strangest thing to me</p>";
+                        echo " <p> But when we touch</p>";
+                        echo " <p> I realize that I found my place in heaven by your side</p>";
+                        echo " <p> Now my life is blessed with the love of an angel</p>";
+                        echo " <p> How can it be true</p>";
+                        echo " <p>Somebody to keep the dream alive </p>";
+                        echo " <p> The dream I found in you</p>";
+                        echo " <p> I always thought the love would be the strangest thing to me</p>";
+                        echo " <p> But when we touch</p>";
+                        echo " <p> I realize that I found my place in heaven by your side</p>";
+                        
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Myracel Diokno</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
