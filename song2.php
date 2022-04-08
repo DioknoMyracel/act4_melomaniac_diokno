766337504ec94c898f1f793140aa5648ae89fd60
DioknoMyracel <myraceldiokno28@gmail.com>
2022-04-09 00:03:35 +0800

    latest commit for song 2

--- song2.php
+++ song2.php
@@ -14,16 +14,16 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> MELOPHILE STATION </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <a class="navbar-brand" href="index.php#page-top"> MELOPHILE STATION </a>
+                <button class="navbar-toggler" type="circle" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="indexto.php">Heaven Knows</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">I'd Rather</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Driver's License</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="song3.php">This City</a></li>
-                          <li class="nav-item"><a class="nav-link text-light" href="song4.php">214</a></li>
-                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Heaven By Your Side</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Heaven Knows</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">I'd Rather</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Driver's License</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">This City</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">214</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song6.php">Heaven By Your Side</a></li>
                     </ul>
                 </div>
             </div>
@@ -44,28 +44,32 @@
                         
                         <?php
 
-                        echo "<h2 class='text-primary'>Driver's  License</h2>";
-                        echo "<p class='lead '>Olivia Rodrigo</p>";
+                        echo "<h2 class='text-primary'>I'd Rather</h2>";
+                        echo "<p class='lead '>Luther Vandross</p>";
                         echo "<p> [Intro] </p>";
-                        echo "<p>I've got my driver's license last week</p>";
-                        echo "<p>Just like we always talked about</p>";
-                        echo "<p>'Cause you were so excited for me</p>";
-                        echo " <p>To finally drive up to your house</p>";
-                        echo " <p>But to day I drove through the suburbs</p>";
-                        echo " <p>Crying 'cause you weren't arounf</p>";
-                        echo " <p>And you're probably with that blond girl</p>";
-                        echo " <p>Who always made me  doubt</p>";
-                        echo " <p>She was so much older than me</p>";
-                        echo " <p>She's everything I'm insecure about</p>";
-                        echo " <p> Yeah, today I drove through the suburds</p>";
-                        echo " <p> 'Cause  how could I ever love someone else</p>";
+                        echo "<p>I thought sometime alone</p>";
+                        echo "<p>Was what we really needed</p>";
+                        echo "<p>You said this time would hurt more than it helps</p>";
+                        echo " <p>But I couldn't see that</p>";
+                        echo " <p>I thought it was the end</p>";
+                        echo " <p>Of a beautiful story</p>";
+                        echo " <p>And so I left the one I love at home to be alone (alone)</p>";
+                        echo " <p>And I tried to find</p>";
+                        echo " <p>Out if this one thing is true</p>";
+                        echo " <p>That I'm nothing without you</p>";
+                        echo " <p>I know better now</p>";
+                        echo " <p> And I've had the change of heart</p>";
                         echo " <p> [Chorus]</p>";
-                        echo " <p> And I know we weren't perfect but I never felt this way for no one</p>";
-                        echo " <p>And I just can't imagine how you could be so okay now that I'm gone</p>";
-                        echo " <p>Guess you didn't mean what you wrote in that song about me </p>";
-                        echo " <p> 'Cause you sade forever now I drove alone past your street</p>";
+                        echo " <p> I'd rather have bad time with you</p>";
+                        echo " <p>Than good times with someone else</p>";
+                        echo " <p>I'd rather be beside you in a storm </p>";
+                        echo " <p> Than safe and sound by myself</p>";
+                        echo " <p> I'd rather have hard times together</p>";
+                        echo " <p> Than to have it easy apart</p>";
+                        echo " <p> I'd rather have the one who holds my heart</p>";
+                        echo " <p> Whoo-oo-oo-oo yeah</p>";
                         echo " <p> [Verse]</p>";
-                        echo " <p> And all my friends are tired</p>";
+                        echo " <p> And then I met someone</p>";
                         echo " <p> And thought she could replace you</p>";
                         echo " <p> We got along just fine</p>";
                         echo " <p> We wasted time, because she was not you</p>";
