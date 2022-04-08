5c059473620ab384427e388b2c3164b32918d678
DioknoMyracel <myraceldiokno28@gmail.com>
2022-04-08 22:27:03 +0800

    commit for song 4

---
+++ song4.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="index.php#page-top"> MELOPHILE STATION </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Heaven Knows</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">I'd Rather</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Driver's License</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">This City</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">214</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song.php">Heaven By Your Side</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h19v class="fw-bolder">Welcome to Melophile Station</h1>
+                <p class="lead">Did you know?? MELOPHILE is the term for those who love music? If you are one of them, then this page is for you. Enjoy! :)</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>This City</h2>";
+                        echo "<p class='lead '>Sam Fischer</p>";
+                        echo "<p> [Intro] </p>";
+                        echo "<p>I've been seeing lonely people in crowded rooms</p>";
+                        echo "<p>Covering their old heartbreaks with new tattoos</p>";
+                        echo "<p>It's all about smoke screens and cigarettes</p>";
+                        echo " <p>Looking through low lights at silhouettes</p>";
+                        echo " <p>But all I see is lonely people in crowded rooms</p>";
+                        echo " <p> [Chorus]</p>";
+                        echo " <p> This city's gonna break my heart</p>";
+                        echo " <p>This city's gonna love me then leave me alone</p>";
+                        echo " <p>This city's got me chasing stars</p>";
+                        echo " <p>It's been a couple months since I felt like I'm home</p>";
+                        echo " <p>Am I getting closer to knowing where I belong?</p> ";
+                        echo " <p>This city's gonna break my heart</p>";
+                        echo " <p> She's always gonna break your heart, oh</p>";
+                        echo " <p> [Verse]</p>";
+                        echo " <p> I remember mornings when my head didn't hurt</p>";
+                        echo " <p> And I remember nights when art didn't feel like work</p>";
+                        echo " <p> She wakes up at noon and she's out 'til three</p>";
+                        echo " <p> She leaves her perfume all over me</p>";
+                        echo " <p> But I remember mornings when my head didn't hurt</p>";
+                        
+                        echo " <p> [Chorus]</p>";
+                        echo " <p>Oh, this city's gonna break my heart(it's gonna break my heart)</p>";
+                        echo " <p>This city's gonna love me then leave me alone</p>";
+                        echo " <p>This city's got me chasing stars(got me chasing stars)</p>";
+                        echo " <p>It's been a couple months since I felt like I'm home(oh)</p>";
+                        echo " <p>Am I getting closer to knowing where I belong?</p> ";
+                        echo " <p>This city's gonna break my heart</p>";
+                        echo " <p> She's always gonna break your heart, oh</p>";
+                        echo " <p> [Verse]</p>";
+                        echo " <p> Ooh-ooh-ooh, ooh, ooh, ooh, ooh, oh</p>";
+                        echo " <p>Ooh-ooh-ooh, ooh, ooh, ooh, ooh</p>";
+                        echo " <p> She got a hold on me</p>";
+                        echo " <p>She got me wrapped 'round her finger</p>";
+                        echo " <p>She got a hold on me</p>";
+                        echo " <p>She got me wrapped 'round her finger</p>";
+                        
+                        echo " <p> [Chorus]</p>";
+                        echo " <p>Oh, this city's gonna break my heart(oh, yeah)</p>";
+                        echo " <p>This city's gonna love me then leave me alone(it's gonna love me then leave me alone)</p>";
+                        echo " <p>This city's got me chasing stars(oh)</p>";
+                        echo " <p>It's been a couple months since I felt like I'm home</p>";
+                        echo " <p>Am I getting closer to knowing where I belong?</p> ";
+                        echo " <p>This city's gonna break my heart(hey  yeah)</p>";
+                        echo " <p> She's always gonna break your heart, oh</p>";
+                        
+                        echo " <p> Ooh-ooh-ooh, ooh, ooh, ooh, ooh, yeah</p>";
+                        echo " <p>Ooh-ooh-ooh, ooh, ooh, ooh, ooh, oh</p>";
+                        echo " <p>This city's gonna break my heart, hey </p>";
+                        echo " <p>She's always gonna break your heart</p>";
+                        
+                        
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Myracel Diokno</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
